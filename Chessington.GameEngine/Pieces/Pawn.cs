6b91c9e91c7c79f964f78b01809551d7be24b0cc
Temuujin Natsagnyam <temuujin.natsagnyam@softwire.com>
2021-10-11 00:51:34 +0100

    r4

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,19 +10,25 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
+            Square next;
+            Square nextNext;
             var square = board.FindPiece(this);
-            if (this.Player == Player.White)
-            {
-                var moves = new List<Square> {Square.At(square.Row - 1, square.Col)};
-                if (!HasMoved) { moves.Add(Square.At(square.Row - 2, square.Col)); }
-                
-                return moves;
+            var moves = new List<Square>();
+
+            if (this.Player == Player.White) {
+                next = Square.At(square.Row - 1, square.Col);
+                nextNext = Square.At(square.Row - 2, square.Col);
+            } else {
+                next = Square.At(square.Row + 1, square.Col);
+                nextNext = Square.At(square.Row + 2, square.Col);
             }
-            
-            var movess = new List<Square> {Square.At(square.Row + 1, square.Col)};  // Why can't i name this moves?
-            if (!HasMoved) { movess.Add(Square.At(square.Row + 2, square.Col)); }
+
+            if (board.GetPiece(next) == null) {
+                moves.Add(next);
+                if (!HasMoved && board.GetPiece(nextNext) == null) { moves.Add(nextNext); }
+            }
                 
-            return movess;
+            return moves;
         }
     }
 }
