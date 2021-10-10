4f317b8b1af74286b0804ae130975dab56d93fcf
Temuujin Natsagnyam <temuujin.natsagnyam@softwire.com>
2021-10-10 23:45:13 +0100

    Pawns can move one square

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,12 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var square = board.FindPiece(this);
+            if (this.Player == Player.White)
+            {
+                return new Square[] {Square.At(square.Row - 1, square.Col)};
+            }
+            return new Square[] {Square.At(square.Row + 1, square.Col)};
         }
     }
 }
